77f21eb67605cc5babfb6e047dba4f09f6128551
Yongyan Liang <yliang@bender.engr.ucr.edu>
2024-04-26 13:32:58 -0700

    reduction change in kernel

--- kernel.cu
+++ kernel.cu
@@ -36,7 +36,22 @@ __global__ void naiveReduction(float *out, float *in, unsigned size)
 
     // INSERT KERNEL CODE HERE
     // NAIVE REDUCTION IMPLEMENTATION
-
+    __shared__ float partialSum[@*BLOCK_SIZE];
+    
+    unsigned int t = threadIdx.x;
+    unsigned int start = 2*blockIdx.x*blockDim.x;
+    
+    partialSum[t] = input[start + t];
+    partialSum[blockDim.x+t] = input[start + blockDim.x+t];
+    
+    for(unsigned int stride = 1;
+      stride <= blockDim.x;  stride *= 2;
+    {
+      
+      __syncthreads();
+      if (t % stride == 0)
+        partialSum[2*t] += partialSum[2*t+stride];
+    }
       
     if(threadIdx.x == 0 && blockIdx.x == 0)
       printWarpDistribution();  
@@ -52,6 +67,22 @@ __global__ void optimizedReduction(float *out, float *in, unsigned size)
 
     // INSERT KERNEL CODE HERE
     // OPTIMIZED REDUCTION IMPLEMENTATION
+    __shared__ float partialSum[@*BLOCK_SIZE];
+    
+    unsigned int t = threadIdx.x;
+    unsigned int start = 2*blockIdx.x*blockDim.x;
+    
+    partialSum[t] = input[start + t];
+    partialSum[blockDim.x+t] = input[start + blockDim.x+t];
+    
+    for(unsigned int stride = 1;
+      stride <= blockDim.x;  stride *= 2;
+    {
+      
+      __syncthreads();
+      if (t % stride == 0)
+        partialSum[2*t] += partialSum[2*t+stride];
+    }
 
     if(threadIdx.x == 0 && blockIdx.x == 0)
       printWarpDistribution();  
